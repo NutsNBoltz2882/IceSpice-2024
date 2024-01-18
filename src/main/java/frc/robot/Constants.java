b7efd45154ef424cc305b70d204e5b692a21213e
NutsNBoltz2882 <149544692+NutsNBoltz2882@users.noreply.github.com>
2024-01-17 22:46:03 -0600

    unfinished swerve module class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,10 +10,30 @@ package frc.robot;
  * globally (i.e. public static). Do not put anything functional in this class.
  *
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * constants are needed, to reduce verbosity
  */
+import static edu.wpi.first.units.Units.*;
+import edu.wpi.first.units.Units;
+
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+
+  public static final class ModuleConstants{
+    //fix all of these dimensions
+    public static final double kWheelDiameterMeters = 0.102; // 4 in converted to m cause units class wont work
+        public static final double kDriveMotorGearRatio = 1 / 5.8462;
+        public static final double kTurningMotorGearRatio = 1 / 18.0;
+        public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
+        public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2 * Math.PI;
+        public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
+        public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
+        public static final double kPTurning = 0.5;
   }
+    public static final class DriveConstants{
+      public static final double kPhyscialMaxSpeedMetersPerSecond = 5;
+
+    }
+
+    public static class OperatorConstants {
+      public static final int kDriverControllerPort = 0;
+    }
 }
--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -3,15 +3,108 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.RobotController;
+//import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.ModuleConstants;
+
 
 public class SwerveModule extends SubsystemBase {
   /** Creates a new SwerveModule. */
-  public SwerveModule() {}
+  private final CANSparkMax driveMotor;
+  private final CANSparkMax turningMotor;
+
+  private final RelativeEncoder driveEncoder;
+  private final RelativeEncoder turningEncoder;
+
+  private final PIDController turningPidController;
+
+  private final AnalogInput absoluteEncoder;
+
+  private final boolean abosoluteEncoderReversed;
+  private final double absoluteEncoderOffsetRad;
+
+  public SwerveModule(int driveMotorId, int turningMotorId, boolean driveMotorReversed, boolean turningMotorReversed,
+  int absoluteEncoderId, double absoluteEncoderOffset, boolean abosoluteEncoderReversed) {
+    this.absoluteEncoderOffsetRad = absoluteEncoderOffset;
+    this.abosoluteEncoderReversed = abosoluteEncoderReversed;
+    absoluteEncoder = new AnalogInput(absoluteEncoderId);
+    
+    driveMotor = new CANSparkMax(driveMotorId, driveMotor.getMotorType()); //if code doesnt work this is prob why
+    turningMotor = new CANSparkMax(turningMotorId, turningMotor.getMotorType());
+
+    driveEncoder = driveMotor.getEncoder();//fill these parameters in tomorrow cause ermm...
+    turningEncoder = turningMotor.getEncoder();//error on lines 44 & 45 r connected to this
+
+
+    driveMotor.setInverted(driveMotorReversed);
+    turningMotor.setInverted(turningMotorReversed);
+
+    driveEncoder.setPositionConversionFactor(ModuleConstants.kDriveEncoderRot2Meter);
+    driveEncoder.setVelocityConversionFactor(ModuleConstants.kDriveEncoderRPM2MeterPerSec);
+    turningEncoder.setPositionConversionFactor(ModuleConstants.kTurningEncoderRot2Rad);
+    turningEncoder.setVelocityConversionFactor(ModuleConstants.kTurningEncoderRPM2RadPerSec);
+
+    turningPidController = new PIDController(ModuleConstants.kPTurning, absoluteEncoderId, absoluteEncoderOffset);
+    turningPidController.enableContinuousInput(-Math.PI, Math.PI);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public double getDrivePosition(){
+    return driveEncoder.getPosition();
+  }
+
+  public double getTurningPosition(){
+    return turningEncoder.getPosition();
+  }
+
+  public double getDriveVelocity(){
+    return driveEncoder.getVelocity();
+  }
+
+  public double getTurningVelocity(){
+    return turningEncoder.getVelocity();
+  }
+
+  public double getAbsoluteEncoderRad(){
+    double angle = absoluteEncoder.getVoltage() / RobotController.getVoltage5V();
+    angle *= 2.0 * Math.PI; 
+    angle -= absoluteEncoderOffsetRad;
+    return angle * (abosoluteEncoderReversed ? -1.0 : 1.0);
+  }
+
+  public void resetEncoders(){
+    driveEncoder.setPosition(0);
+    turningEncoder.setPosition(getAbsoluteEncoderRad());
+  }
+
+  public SwerveModuleState getState(){
+    return new SwerveModuleState(getDriveVelocity(), new Rotation2d(getTurningPosition()));
+  }
+
+  public void setDesiredState(SwerveModuleState state){
+    if(Math.abs(state.speedMetersPerSecond) < 0.001){
+      stop(); //prob just renamed, update to 24 version
+      return;
+    }
+    state = SwerveModuleState.optimize(state, getState().angle);
+    driveMotor.set(state.speedMetersPerSecond / DriveConstants.kPhyscialMaxSpeedMetersPerSecond);
+    turningMotor.set(turningPidController.calculate(getTurningPosition(), state.angle.getRadians()));
+    SmartDashboard.putString("Swerve[" + absoluteEncoder.getChannel() + "] state", state.toString());
   }
 }
