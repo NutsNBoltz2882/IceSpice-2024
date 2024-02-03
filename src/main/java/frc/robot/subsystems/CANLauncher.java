db3a2449930bbd1dc1d215920afe9c465f4533af
Vasska <vasskae@gmail.com>
2024-02-03 14:51:06 -0600

    Fixed some stuff

--- src/main/java/frc/robot/subsystems/CANLauncher.java
+++ src/main/java/frc/robot/subsystems/CANLauncher.java
@@ -7,21 +7,21 @@ package frc.robot.subsystems;
 import static frc.robot.Constants.LauncherConstants.*;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+//import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CANLauncher extends SubsystemBase {
-  CANSparkMax m_launchWheel;
-  CANSparkMax m_feedWheel;
+  CANSparkMax leftWheels;
+  CANSparkMax rightWheels;
 
   /** Creates a new Launcher. */
   public CANLauncher() {
-    m_launchWheel = new CANSparkMax(kLauncherID, MotorType.kBrushed);
-    m_feedWheel = new CANSparkMax(kFeederID, MotorType.kBrushed);
+    leftWheels = new CANSparkMax(kLeftID, MotorType.fromId(kLeftID) );
+    rightWheels = new CANSparkMax(kRightID, MotorType.kBrushed);
 
-    m_launchWheel.setSmartCurrentLimit(kLauncherCurrentLimit);
-    m_feedWheel.setSmartCurrentLimit(kFeedCurrentLimit);
+    leftWheels.setSmartCurrentLimit(kLauncherCurrentLimit);
+    rightWheels.setSmartCurrentLimit(kFeedCurrentLimit);
   }
 
   /**
@@ -48,18 +48,18 @@ public class CANLauncher extends SubsystemBase {
 
   // An accessor method to set the speed (technically the output percentage) of the launch wheel
   public void setLaunchWheel(double speed) {
-    m_launchWheel.set(speed);
+    leftWheels.set(speed);
   }
 
   // An accessor method to set the speed (technically the output percentage) of the feed wheel
   public void setFeedWheel(double speed) {
-    m_feedWheel.set(speed);
+    rightWheels.set(speed);
   }
 
   // A helper method to stop both wheels. You could skip having a method like this and call the
   // individual accessors with speed = 0 instead
   public void stop() {
-    m_launchWheel.set(0);
-    m_feedWheel.set(0);
+    leftWheels.set(0);
+    rightWheels.set(0);
   }
 }
